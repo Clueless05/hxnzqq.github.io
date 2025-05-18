3baefa96cc9ab59a6014c7605cb649a0d52fe5e7
Hxnzqq <158502215+Clueless05@users.noreply.github.com>
2025-05-19 01:39:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,19 @@ document.addEventListener('DOMContentLoaded', function() {
         starContainer.appendChild(star);
         animateStar(star);
     }
+    /*   
+ /$$                   /$$                        ©
+| $$                  | $$                        
+| $$$$$$$  /$$   /$$ /$$$$$$    /$$$$$$   /$$$$$$ 
+| $$__  $$| $$  | $$|_  $$_/   /$$__  $$ /$$__  $$
+| $$  \ $$| $$  | $$  | $$    | $$  \__/| $$  \ $$
+| $$  | $$| $$  | $$  | $$ /$$| $$      | $$  | $$
+| $$  | $$|  $$$$$$$  |  $$$$/| $$      |  $$$$$$/
+|__/  |__/ \____  $$   \___/  |__/       \______/ 
+           /$$  | $$                              
+          |  $$$$$$/                              
+           \______/                               
+What are you doing here? Don't copy my work! Any attempt to make a copy is considered theft.*/
 
     function animateStar(star) {
         const speed = Math.random() * 10 + 5; 
