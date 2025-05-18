974506288998e4522fa686d5113e1b0abc59b45f
Hxnzqq <158502215+Clueless05@users.noreply.github.com>
2025-05-19 01:39:59 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,16 @@
+/*   
+ /$$                   /$$                        ©
+| $$                  | $$                        
+| $$$$$$$  /$$   /$$ /$$$$$$    /$$$$$$   /$$$$$$ 
+| $$__  $$| $$  | $$|_  $$_/   /$$__  $$ /$$__  $$
+| $$  \ $$| $$  | $$  | $$    | $$  \__/| $$  \ $$
+| $$  | $$| $$  | $$  | $$ /$$| $$      | $$  | $$
+| $$  | $$|  $$$$$$$  |  $$$$/| $$      |  $$$$$$/
+|__/  |__/ \____  $$   \___/  |__/       \______/ 
+           /$$  | $$                              
+          |  $$$$$$/                              
+           \______/                               
+What are you doing here? Don't copy my work! Any attempt to make a copy is considered theft.*/
 document.addEventListener('DOMContentLoaded', () => {
     const components = document.querySelectorAll('.pc-component');
 
